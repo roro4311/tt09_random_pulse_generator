eabae39c5f8ee2919be9f44c15b390f482acd523
roro4311 <41744527+roro4311@users.noreply.github.com>
2024-11-08 23:08:09 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -6,7 +6,6 @@ module tb;
     // Testbench signals
     reg clk;                // Clock input
     reg rst_n;              // Active low reset
-    reg ena;
     wire [7:0] uo_out;      // Output (unused in this test)
     wire [7:0] uio_out;     // Output for pulse signal (uio_out[0] for pulse)
     wire [7:0] uio_oe;      // Output enable signal (for uio_out)
@@ -16,7 +15,6 @@ module tb;
     tt_um_random_pulse_generator uut (
         .clk(clk),
         .rst_n(rst_n),
-        .ena,
         .uo_out(uo_out),
         .uio_out(uio_out),
         .uio_oe(uio_oe),
